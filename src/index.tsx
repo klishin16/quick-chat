4d4cce393382a28a12c11fd44d65850facf26423
Nikita <klishin.nd16@gmail.com>
2021-10-04 18:59:05 +0300

    Realize 0.1 fix 3

--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,25 @@ import reportWebVitals from './reportWebVitals';
 import firebase from "firebase/compat";
 import {AuthProvider} from "./provider/AuthProvider";
 
-// const {
-//     REACT_APP_FIREBASE_API_KEY,
-//     REACT_APP_AUTH_DOMAIN,
-//     REACT_APP_DATABASE_URL,
-//     REACT_APP_PROJECT_ID,
-//     REACT_APP_STORAGE_BUCKET,
-//     REACT_APP_MESSAGING_SENDER_ID,
-//     REACT_APP_APP_ID,
-//     REACT_APP_MEASUREMENT_ID,
-// } = process.env
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_AUTH_DOMAIN,
+    REACT_APP_DATABASE_URL,
+    REACT_APP_PROJECT_ID,
+    REACT_APP_STORAGE_BUCKET,
+    REACT_APP_MESSAGING_SENDER_ID,
+    REACT_APP_APP_ID,
+    REACT_APP_MEASUREMENT_ID,
+} = process.env
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMkiWcgn2RHGINdze6WhAzgZIFtX18A7U",
-    authDomain: "quick-chat-8d3bf.firebaseapp.com",
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
     // databaseURL: REACT_APP_DATABASE_URL,
-    projectId: "quick-chat-8d3bf",
-    storageBucket: "quick-chat-8d3bf.appspot.com",
-    messagingSenderId: "153531162448",
-    appId: "1:153531162448:web:33e29f331e8c926fea5655",
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
     // measurementId: REACT_APP_MEASUREMENT_ID
 };
 console.log('NODE_ENV:', process.env.NODE_ENV)
